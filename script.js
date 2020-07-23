45f2fd13c042a499222cf0a8b32f6d68a0205288
Brandon <codebva@gmail.com>
2020-07-22 21:56:52 -0700

    Update script.js
    replaced parseInt() with parseFloat

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
     let outputBar = $('#output');
     // Declaring new var for value of current output when
     // an .operator btn is clicked
-    let firstInput = 0;
+    let firstInput = 0.00;
     // declaring new var for value of second output
-    let secondOutput = 0;
+    let secondOutput = 0.00;
     // declaring var for opreator value 
     let operatorValue = 0;
 
@@ -19,12 +19,14 @@ $(document).ready(function() {
 
         // checking if current value of output bar is 0
         // if true set to empty stirng
-        if (outputBar.text() === '0'){ 
+        if (outputBar.text() === '0' && btnNumberValue != '.'){ 
             outputBar.text('');
         }
+        
 
         // appending the value of the pressed btn to the current output bar
         outputBar.append(btnNumberValue);   
+        
     })
 
     // when an operator is clicked...
@@ -36,6 +38,7 @@ $(document).ready(function() {
         // when an operator is clicked it stores current value
         // of #outputBar as a new var
         firstInput = $('#output').text();
+        // firstInput = parseInt(firstInput);
 
         // creating a function to to test the type of 
         // operator and store its value in operatorValue var
@@ -47,7 +50,7 @@ $(document).ready(function() {
             } else if (operator === '*') {
                 operatorValue = '*';
             } else if (operator === '/') {
-                operatorValue = '/'
+                operatorValue = '/';
             }
         }
         // Calling doSomeMathFunction
@@ -55,34 +58,47 @@ $(document).ready(function() {
 
         // Setting #outputBar back to 0.
         outputBar.text('0');
+        console.log(firstInput);
+        console.log(outputBar);
     })
 
     // When = is clicked...
     $('#btnEnter').click( function() {
-
+        
         // Storing value of current output to secondOutput.
         secondOutput = $('#output').text();
         // Converting first and second output into numbers.
-        firstInput = parseInt(firstInput);
-        secondOutput = parseInt(secondOutput);
+        firstInput = parseFloat(firstInput);
+        secondOutput = parseFloat(secondOutput);
+
         // Creating a var for the finalValue.
         let finalValue = 0;
 
         // Creating if else statement to operatorValue.
         if (operatorValue === '+') {
             // If true add.
+            // secondOutput = parseInt(secondOutput);
+            console.log(firstInput);
+            console.log(secondOutput);
+            
             finalValue = firstInput + secondOutput;
         } else if (operatorValue === '-') {
             // If true subtract.
+            // secondOutput = parseInt(secondOutput);
             finalValue = firstInput - secondOutput;
         } else if (operatorValue === '*') {
             // If true multiply.
             finalValue = firstInput * secondOutput;
         } else if (operatorValue === '/') {
             // If true divide.
+            console.log(firstInput);
+            console.log(secondOutput);
            finalValue = firstInput / secondOutput;
+           console.log(finalValue);
         }
 
+        // firstInput = parseInt(firstInput);
+        // secondOutput = parseInt(secondOutput);
         // Outputing the finalValue to outputBar.
         $("#output").text(finalValue);
     })
@@ -117,5 +133,6 @@ $(document).ready(function() {
         }
     })
 });
+
 
 
